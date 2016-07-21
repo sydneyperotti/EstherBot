929acfce157686a052d50109236794fdb58f7da8
sydneyperotti <sydney.perotti@fcb.com>
2016-07-21 11:56:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Sydney? Just say HELLO to get started.')
+            return bot.say('Welcome to the Clorox MyStain ChatBot!)
                 .then(() => 'speak');
         }
     },
